778433bd18765076694936112d03e9385e3457c8
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-22 13:51:42 -0800

    httparty module is required in updates controller and method get_updates is created to fetch api data, data is successfully printed on terminal

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,5 +1,15 @@
+require "httparty"
+require "json"
+
 class UpdatesController < ApplicationController
 
   def index
+    get_updates
+  end
+
+  def get_updates
+    url = "https://raw.githubusercontent.com/Vericatch/devtestapiapp/master/mock_response.json"
+    @response = HTTParty.get(url)
+    puts @response
   end
 end
