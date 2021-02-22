35acdf2ccab2e923d265554716e6a44cb6843cef
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-22 14:23:55 -0800

    removed the logics fromindex.html.erb and abstracted it into a method in updates controller and utilize it in index action

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -4,11 +4,16 @@ require "json"
 class UpdatesController < ApplicationController
 
   def index
-    @updates = get_updates
+    @all_updates = get_updates
+    @filtered_updates = @all_updates.select{|update| includes_keyword?(update)}
   end
 
   def get_updates
     url = "https://raw.githubusercontent.com/Vericatch/devtestapiapp/master/mock_response.json"
     @response = JSON.parse(HTTParty.get(url))
+  end
+
+  def includes_keyword?(update)
+    update["message"].include? "coke"  or  update["message"].include? "coca-cola" or update["message"].include? "diet-cola"
   end
 end
