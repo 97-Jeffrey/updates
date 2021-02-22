f93ccdb70f0dbfa9368fa22e7855fee40607eb71
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-22 15:09:12 -0800

    comments add to explainfunctionality of each method/action

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -3,6 +3,7 @@ require "json"
 
 class UpdatesController < ApplicationController
 
+  # All logics of sorting, filtering, and counting are implemented by controllers' methods, then passed in as variables for index page to render, so index.html.erb is just for render html and variables.
   def index
     @all_updates = get_updates
     @filtered_updates = @all_updates.select{|update| includes_keyword?(update)}.sort_by{|update| update["sentiment"]}
@@ -11,11 +12,13 @@ class UpdatesController < ApplicationController
     @percentage = (@count.to_f/@all_updates.length)*100
   end
 
+  # This method is for retrieving all updates from API
   def get_updates
     url = "https://raw.githubusercontent.com/Vericatch/devtestapiapp/master/mock_response.json"
     @response = JSON.parse(HTTParty.get(url))
   end
 
+  #  Numbers of updates with keywords:
   def count(updates)
     keyword_count = 0;
     updates.each do |update|
@@ -26,6 +29,7 @@ class UpdatesController < ApplicationController
     keyword_count
   end
 
+  # filtering conditional:
   def includes_keyword?(update)
     update["message"].include? "coke"  or  update["message"].include? "coca-cola" or update["message"].include? "diet-cola"
   end
