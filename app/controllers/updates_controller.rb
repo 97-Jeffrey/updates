7efd4d8e38e42de5707b490a8be08b34e400a14c
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-22 14:48:40 -0800

    a method count is implemented to calculate how many updates with keyword

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -6,11 +6,24 @@ class UpdatesController < ApplicationController
   def index
     @all_updates = get_updates
     @filtered_updates = @all_updates.select{|update| includes_keyword?(update)}.sort_by{|update| update["sentiment"]}
+
+    @count = count(@filtered_updates)
+    
   end
 
   def get_updates
     url = "https://raw.githubusercontent.com/Vericatch/devtestapiapp/master/mock_response.json"
     @response = JSON.parse(HTTParty.get(url))
+  end
+
+  def count(updates)
+    keyword_count = 0;
+    updates.each do |update|
+      if includes_keyword?(update)
+        keyword_count +=1
+      end
+    end
+    keyword_count
   end
 
   def includes_keyword?(update)
